7bc591e7e92fc9e0ee8dfa11ee27251db3c68e75
Rohit Singh <rajr32959@gmail.com>
2024-08-08 00:52:43 +0530

    Pushing files

---
+++ src/main/java/com/rohit/quizapp/controller/QuestionController.java
@@ -0,0 +1,48 @@
+package com.rohit.quizapp.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.rohit.quizapp.Question;
+import com.rohit.quizapp.service.QuestionService;
+
+
+@RestController
+@RequestMapping("question")
+public class QuestionController {
+    @Autowired
+    QuestionService questionService;
+    @GetMapping("allQuestions")
+    public ResponseEntity<List<Question>> getAllQuestions(){
+        return questionService.getAllQuestions();
+        
+    }
+    @GetMapping("category/{category}")
+    public ResponseEntity<List<Question>> getQuestionsByCattegory(@PathVariable String category){
+        return questionService.getQuestionsByCattegory(category);
+
+    }
+    @PostMapping("add")
+    public ResponseEntity<String> addQuestions(@RequestBody Question question){
+        return questionService.addQuestions(question);
+    }
+    @DeleteMapping("delete")
+    public ResponseEntity<String> deleteQuestion(@RequestParam Integer Id){
+        return questionService.deleteQuestion(Id);
+    }
+    @PutMapping("update")
+    public ResponseEntity<String>updateQuestion(@RequestParam Integer Id, @RequestBody Question question){
+        return questionService.updateQuestion(Id, question);
+    }
+}
---
+++ src/main/java/com/rohit/quizapp/service/QuestionService.java
@@ -0,0 +1,85 @@
+package com.rohit.quizapp.service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+//import org.apache.el.stream.Optional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import com.rohit.quizapp.Question;
+import com.rohit.quizapp.dao.QuestionDao;
+
+@Service
+public class QuestionService {
+    @Autowired
+    QuestionDao questionDao;
+    public ResponseEntity<List<Question>> getAllQuestions() {
+        try{
+            return new ResponseEntity<>(questionDao.findAll(),HttpStatus.OK);
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+        return new ResponseEntity<>(new ArrayList<>(),HttpStatus.BAD_REQUEST);
+    }
+    public ResponseEntity<List<Question>> getQuestionsByCattegory(String category) {
+        try{
+            return new ResponseEntity<>(questionDao.findByCategory(category),HttpStatus.OK);
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+        return new ResponseEntity<>(new ArrayList<>(),HttpStatus.BAD_REQUEST);
+    }
+    public ResponseEntity<String> addQuestions(Question question) {
+        try{
+            questionDao.save(question);
+            return new ResponseEntity<>("question added successfully",HttpStatus.CREATED);
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+        return new ResponseEntity<>("Not able to add question",HttpStatus.BAD_REQUEST);
+    }
+    public ResponseEntity<String> deleteQuestion(Integer Id) {
+        try{
+            Optional<Question> question=questionDao.findById(Id);
+            if(question.isPresent()){
+            questionDao.deleteById(Id);
+                return new ResponseEntity<>("deleted Successfully",HttpStatus.OK);
+            }
+            else{
+                return new ResponseEntity<>("question not found",HttpStatus.NOT_FOUND);
+            }
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+        return new ResponseEntity<>("An error occured while deleting the question",HttpStatus.INTERNAL_SERVER_ERROR); 
+    }
+    public ResponseEntity<String> updateQuestion(Integer Id, Question updatedQuestion) {
+        try{
+            Optional<Question> question=questionDao.findById(Id);
+            if(question.isPresent()){
+                Question existingQuestion=question.get();
+                existingQuestion.setCategory(updatedQuestion.getCategory());
+                existingQuestion.setQuestionTitle(updatedQuestion.getQuestionTitle());
+                existingQuestion.setDifficultyLevel(updatedQuestion.getDifficultyLevel());
+                existingQuestion.setOption1(updatedQuestion.getOption1());
+                existingQuestion.setOption2(updatedQuestion.getOption2());
+                existingQuestion.setOption3(updatedQuestion.getOption3());
+                existingQuestion.setOption4(updatedQuestion.getOption4());
+                existingQuestion.setRightAnswer(updatedQuestion.getRightAnswer());
+                questionDao.save(existingQuestion);
+                return new ResponseEntity<>("Updated Successfully",HttpStatus.ACCEPTED);
+            }
+            else{
+                return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Questio not found");
+            }
+        }catch(Exception e){
+            return new ResponseEntity<>("Something went wrong while updating question",HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+    
+    
+}
